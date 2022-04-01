06a286ec71ab61ba75e2b786aa15ffa7a9839445
Kirill Prikhodkin <kirill@prikhodkin.com>
2022-04-01 23:24:56 +0500

    Type registration

--- js/main.js
+++ js/main.js
@@ -11,11 +11,41 @@ var swiper = new Swiper(".mySwiper", {
 
   const menuButton = document.querySelector('.menu-btn')
   const menu = document.querySelector('.menu-slide')
+  const menuitem = document.querySelector('.menu__item')
 
   menuButton.addEventListener('click', function (event) {
     event.preventDefault()
+
     
     this.classList.toggle('menu-btn_active')
     menu.classList.toggle('menu-slide__active')
+    
   });
 
+
+
+$('.quantity__inner .btn__minus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+});
+
+$('.quantity__inner .btn__plus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+}); 
+
+$('.quantity__inner .quantity').bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+        this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+    }    
+}); 
